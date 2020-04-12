21d3b782ef47659235fb5b3f700130fbeddb34dc
Yunus A Mulla <myounis.ar@live.com>
2020-04-12 12:52:08 +0530

    Sync and list items in cart

--- src/components/Cart/cart.component.jsx
+++ src/components/Cart/cart.component.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import '../../assets/styles/cart.scss';
 
 import CustomButton from '../Base/Button/button.component';
+import CartItem from '../CartItem/cartItem.component';
 
-export default class Cart extends Component {
+class Cart extends Component {
+    constructor (props) {
+        super(props);
+    }
+    
     render() {
         return (
             <div className='cart-dropdown'>
-                <div className="cart-items" />
+                {this.props.cartItems.length > 0 && <div className="cart-items"> 
+                        {
+                            this.props.cartItems.map((item) => {
+                                return <CartItem key={item.id} item={item}/>
+                            })
+                        }
+                    </div>
+                }
+
+                {this.props.cartItems.length === 0 &&
+                    <span className='no-items'> No items in cart </span>
+                }
+
                 <CustomButton>CHECKOUT</CustomButton>
             </div>
         )
     }
 }
+
+const mapStateToProps = ({ cart }) => ({
+    cartItems: cart.cartItems
+});
+
+export default connect(
+    mapStateToProps
+)(Cart);
---
+++ src/components/CartItem/cartItem.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../assets/styles/cart-item.scss';
+
+export default function CartItem(props) {
+    let { item } = props;
+
+    return (
+        <div className='cart-item'>
+            <img src={item.imageUrl} alt='item'/>
+            <div className="item-details">
+                <span className="name">{item.name}</span>
+                <span className="price">$ {item.price * item.count}</span>
+            </div>
+        </div>
+    )
+}
